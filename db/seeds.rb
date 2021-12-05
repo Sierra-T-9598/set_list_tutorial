36441ce40d7729b62dc9a31b66ba51563f8e694b
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-05 10:30:15 -0700

    Adds artist new and create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 prince = Artist.create!(name: 'Prince')
+taylor_swift = Artist.create!(name: 'Taylor Swift')
 purple = prince.songs.create!(title: 'Purple Rain', length: 437, play_count: 3498)
 beret = prince.songs.create!(title: 'Raspberry Beret', length: 427, play_count: 5498)
+all_to_well = taylor_swift.songs.create!(title: 'All to Well', length: 500, play_count: 34)
+mean = taylor_swift.songs.create!(title: 'Mean', length: 200, play_count: 490)
